6d2c626039ceb28c3b63013c1cba03a0ebda567d
Terrorize420 <34874582+Terrorize420@users.noreply.github.com>
2018-01-02 16:17:32 -0800

    update sources
    update

--- script.terrorizeinc/addon.py
+++ script.terrorizeinc/addon.py
@@ -4,8 +4,8 @@ import xbmcgui
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
  
-line1 = "Hello Brad The Butt Baby!"
-line2 = "Hows Is Going?"
-line3 = "Well OK Bye Now Butt Baby"
+line1 = "[COLOR gold]Hello[/COLOR] [COLOR blue]Brad[/COLOR][COLOR gold] The Butt Baby![/COLOR]"
+line2 = "[COLOR red]Hows Is Going?[/COLOR]"
+line3 = "[COLOR freen]Well OK Bye Now Butt Baby[/COLOR]"
  
 xbmcgui.Dialog().ok(addonname, line1, line2, line3)
